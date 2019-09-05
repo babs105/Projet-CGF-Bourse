93e0741be6d4a4384f041dd929bdab6b9e904834
Babacar Dieng <mamembaye105@live.fr>
2019-09-05 11:19:47 +0100

    second commit

--- src/assets/params/js/index.js
+++ src/assets/params/js/index.js
@@ -3,4 +3,12 @@ $(function () {
     navMain.on("click", "a", null, function () {
         navMain.collapse('hide');
     });
+    $("#inscrire").click(function () {
+        $('#myModalConnexion').modal('hide');
+        $('#myModalInscription').modal('show');
+    });
+    $("#connecter").click(function () {
+        $('#myModalInscription').modal('hide');
+        $('#myModalConnexion').modal('show');
+    });
 });
